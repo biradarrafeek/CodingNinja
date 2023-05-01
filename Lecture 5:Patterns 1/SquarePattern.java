b79d15d2b84afe5c9878b15a7c1a28d00d286069
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-05-01 15:55:23 +0530

    Update SquarePattern.java

--- Lecture 5:Patterns 1/SquarePattern.java
+++ Lecture 5:Patterns 1/SquarePattern.java
@@ -1,1 +1,22 @@
+package Patterns1;
 
+import java.util.Scanner;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		int i=1;
+		while(i<=n) {
+			int j=1;
+			while(j<=n) {
+				System.out.print(n);
+				j=j+1;
+			}
+			System.out.println();
+			i=i+1;
+		}
+	}
+}
