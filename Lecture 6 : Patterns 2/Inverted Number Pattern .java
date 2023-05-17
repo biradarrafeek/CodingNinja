c2fed0e3565de136aac23fc2fd769a34ec7f0767
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-05-17 10:05:25 +0530

    Create Inverted Number Pattern .java

---
+++ Lecture 6 : Patterns 2/Inverted Number Pattern .java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        while(i <= n){
+            int j = n-i+1;
+            int num = j;
+            while(j >0 ){
+                System.out.print(num);
+                j--;
+            }
+            System.out.println();
+            i++;
+        }
+
+    }
+
+}
